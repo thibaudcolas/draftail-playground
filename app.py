bf7b86ec2c5ccfb02fb97b6f2605d7f2977fdb3b
Thibaud Colas <thibaudcolas@gmail.com>
2016-11-18 12:31:18 +0200

    Fix app port binding

--- app.py
+++ app.py
@@ -91,4 +91,6 @@ def static_file(path):
 
 
 if __name__ == '__main__':
-    app.run(port=os.environ.get('PORT', 5000))
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
