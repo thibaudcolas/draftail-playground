8a1013163b82a5709251d7a7d68a7d2baf7e3152
Thibaud Colas <thibaudcolas@gmail.com>
2017-01-17 22:53:18 +0200

    Update to latest draftail version

--- src/index.js
+++ src/index.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import DraftailEditor from 'draftail';
-import './wagtail-icons.css';
-import './draftail.css';
+import DraftailEditor, { BLOCK_TYPE, INLINE_STYLE } from 'draftail';
+import 'draftail/dist/draftail.css';
 import './index.css';
 
-const $ = window.jQuery;
+const postRequest = (endpoint, data, successCallback) => {
+    const request = new XMLHttpRequest();
+    request.open('POST', endpoint, true);
+    request.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+    request.onload = () => {
+        if (request.status >= 200 && request.status < 400) {
+            successCallback(request.responseText);
+        }
+    };
+    request.send(data);
+};
 
 const displayResult = (contentState) => {
-    $.ajax({
-        type: 'POST',
-        url: '/api/export',
-        contentType: 'application/json',
-        data: JSON.stringify(contentState),
-        success: (html) => {
-            document.querySelector('[data-export]').innerHTML = html;
-            document.querySelector('[data-code]').innerHTML = JSON.stringify(contentState, null, 4);
-        },
+    postRequest('/api/export', JSON.stringify(contentState), (html) => {
+        document.querySelector('[data-export]').innerHTML = html;
+        document.querySelector('[data-code]').innerHTML = JSON.stringify(contentState, null, 4);
     });
 };
 
@@ -51,25 +54,17 @@ ReactDOM.render((
     <DraftailEditor
         rawContentState={defaultContentState}
         onSave={onSave}
-        options={{
-            enableHorizontalRule: true,
-            enableLineBreak: true,
-            modelPickerOptions: [],
-            imageFormats: [],
-            mediaControls: [],
-            dialogControls: [],
-            sources: [],
-            decorators: [],
-            BLOCK_TYPES: [
-                { label: 'H2', style: 'header-two' },
-                { label: 'H3', style: 'header-three' },
-                { label: 'UL', style: 'unordered-list-item', icon: 'list-ul' },
-                { label: 'OL', style: 'ordered-list-item', icon: 'list-ol' },
-            ],
-            INLINE_STYLES: [
-                { label: 'Bold', style: 'BOLD', icon: 'bold' },
-                { label: 'Italic', style: 'ITALIC', icon: 'italic' },
-            ],
-        }}
+        enableHorizontalRule
+        enableLineBreak
+        blockTypes={[
+            { label: 'H2', type: BLOCK_TYPE.HEADER_TWO },
+            { label: 'H3', type: BLOCK_TYPE.HEADER_THREE },
+            { label: 'UL', type: BLOCK_TYPE.UNORDERED_LIST_ITEM, icon: 'icon-list-ul' },
+            { label: 'OL', type: BLOCK_TYPE.ORDERED_LIST_ITEM, icon: 'icon-list-ol' },
+        ]}
+        inlineStyles={[
+            { label: 'Bold', type: INLINE_STYLE.BOLD, icon: 'icon-bold' },
+            { label: 'Italic', type: INLINE_STYLE.ITALIC, icon: 'icon-italic' },
+        ]}
     />
 ), document.querySelector('[data-mount]'));
