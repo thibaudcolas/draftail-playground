d0afb7ca1a04c6c4e2397de007189e44b9cd538d
Thibaud Colas <thibaudcolas@gmail.com>
2017-05-30 00:24:24 +0300

    Add service worker from create-react-app to the demo (#8)

--- src/index.js
+++ src/index.js
@@ -8,5 +8,8 @@ import './index.css';
 import './icomoon/styles.css';
 
 import App from './components/App';
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.querySelector('[data-mount]'));
+
+registerServiceWorker();
