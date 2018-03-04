c9814415e99db6fdccd67898880647f2b02c9c65
Thibaud Colas <thibaudcolas@gmail.com>
2018-03-04 13:20:41 +0200

    Add resizable panel

--- src/components/SidePanel.js
+++ src/components/SidePanel.js
@@ -1,22 +1,61 @@
+// @flow
+import React from "react";
 import styled from "styled-components";
+import { ResizableBox, Resizable } from "react-resizable";
+import type { Node } from "react";
 
-const SidePanel = styled.div`
-  --min-width: 320px;
+import "./SidePanel.css";
 
-  width: 40%;
-  min-width: var(--min-width);
+// Remove propTypes declaration preventing 100% height.
+delete Resizable.propTypes;
+delete ResizableBox.propTypes;
+
+const VIEWPORT_WIDTH = document.documentElement
+  ? document.documentElement.clientWidth
+  : 1024;
+const MIN_PANEL_WIDTH = 320;
+const MIN_PREVIEW_WIDTH = 320;
+const MAX_PANEL_WIDTH =
+  VIEWPORT_WIDTH - (VIEWPORT_WIDTH > 768 ? MIN_PREVIEW_WIDTH : 0);
+
+console.log(MAX_PANEL_WIDTH);
+
+const savedWidth = Number(window.sessionStorage.getItem("panel-width"));
+const initWidth = savedWidth || Math.max(MIN_PANEL_WIDTH, VIEWPORT_WIDTH * 0.4);
+
+const saveWidth = (
+  e: SyntheticEvent<EventTarget>,
+  { size }: { size: { width: number, height: number } },
+) => {
+  window.sessionStorage.setItem("panel-width", size.width);
+};
+
+const Container = styled.div`
   padding: 1rem;
 
   @media screen and (min-width: 768px) {
     min-height: 100vh;
-    border-right: 1px solid #333;
+    border-right: 1px double #333;
     padding: 2rem;
     --min-width: 420px;
   }
-
-  @media screen and (min-width: 1280px) {
-    --min-width: 600px;
-  }
 `;
+
+type Props = {
+  children: Node,
+};
+
+const SidePanel = ({ children }: Props) => (
+  <ResizableBox
+    width={initWidth}
+    height="100%"
+    axis="x"
+    minConstraints={[MIN_PANEL_WIDTH, "100%"]}
+    maxConstraints={[MAX_PANEL_WIDTH, "100%"]}
+    onResizeStop={saveWidth}
+  >
+    <Container>{children}</Container>
+  </ResizableBox>
+);
 
 export default SidePanel;
