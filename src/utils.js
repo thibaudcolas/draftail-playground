efff00d1770ef81bb6fe9964060fade1f2faee67
Thibaud Colas <thibaudcolas@gmail.com>
2018-03-07 14:29:03 +0200

    Update demo default content

--- src/utils.js
+++ src/utils.js
@@ -12,42 +12,56 @@ export const postRequest = (endpoint, data, successCallback) => {
 
 // eslint-disable-next-line
 export const defaultContentState = {
-  entityMap: {
-    "0": {
-      type: "LINK",
-      mutability: "MUTABLE",
-      data: { url: "https://springload.github.io/draftail/" },
-    },
-    "1": {
-      type: "LINK",
-      mutability: "MUTABLE",
-      data: { url: "https://github.com/springload/draftjs_exporter" },
-    },
-    "2": {
-      type: "IMAGE",
-      mutability: "IMMUTABLE",
-      data: {
-        altText: "Test image alt text",
-        alignment: "left",
-        src: "https://placekitten.com/g/260/160",
-      },
-    },
-  },
   blocks: [
     {
       key: "b1ito",
-      text: "Draftail works well with draftjs_exporter!",
-      type: "unstyled",
+      text: "Why Wagtail’s new editor is built with Draft.js",
+      type: "header-one",
       depth: 0,
-      inlineStyleRanges: [],
-      entityRanges: [
-        { offset: 0, length: 8, key: 0 },
-        { offset: 25, length: 16, key: 1 },
-      ],
+      inlineStyleRanges: [{ offset: 0, length: 47, style: "BOLD" }],
+      entityRanges: [],
       data: {},
     },
     {
-      key: "f1nl8",
+      key: "ba7s7",
+      text:
+        "Have you ever worked with rich text editors? You know how frustrating of an experience that can be. Users enter content without much confidence in how the editor will preserve it. Developers have to harden their sites to handle the unstructured soup that is rich text. Designers see their layout completely destroyed by the cheer variation in shape and length. A recipe for frustration.",
+      type: "unstyled",
+      depth: 0,
+      inlineStyleRanges: [{ offset: 361, length: 25, style: "BOLD" }],
+      entityRanges: [],
+      data: {},
+    },
+    {
+      key: "67jbt",
+      text: " ",
+      type: "atomic",
+      depth: 0,
+      inlineStyleRanges: [],
+      entityRanges: [{ offset: 0, length: 1, key: 0 }],
+      data: {},
+    },
+    {
+      key: "fe44d",
+      text:
+        "It’s time to add some noodles and flavour, give some structure to our bland broth and enjoy rich text again.",
+      type: "unstyled",
+      depth: 0,
+      inlineStyleRanges: [],
+      entityRanges: [{ offset: 43, length: 38, key: 1 }],
+      data: {},
+    },
+    {
+      key: "69loq",
+      text: "",
+      type: "unstyled",
+      depth: 0,
+      inlineStyleRanges: [],
+      entityRanges: [],
+      data: {},
+    },
+    {
+      key: "91i32",
       text: " ",
       type: "atomic",
       depth: 0,
@@ -56,33 +70,49 @@ export const defaultContentState = {
       data: {},
     },
     {
-      key: "crmq4",
-      text: "List item",
-      type: "unordered-list-item",
+      key: "57k2l",
+      text: "Introducing Draft.js",
+      type: "header-two",
       depth: 0,
       inlineStyleRanges: [],
       entityRanges: [],
       data: {},
     },
     {
-      key: "c4l1f",
-      text: "Nested",
-      type: "unordered-list-item",
-      depth: 1,
-      inlineStyleRanges: [{ offset: 0, length: 6, style: "ITALIC" }],
-      entityRanges: [],
-      data: {},
-    },
-    {
-      key: "3onpa",
-      text: "and back",
-      type: "unordered-list-item",
+      key: "384sd",
+      text:
+        "Draft.js is a framework for building rich text editors in React, powered by an immutable model and abstracting over cross-browser differences.",
+      type: "blockquote",
       depth: 0,
       inlineStyleRanges: [],
-      entityRanges: [],
+      entityRanges: [{ offset: 0, length: 8, key: 3 }],
       data: {},
     },
   ],
+  entityMap: {
+    "0": { type: "HORIZONTAL_RULE", mutability: "IMMUTABLE", data: {} },
+    "1": {
+      type: "LINK",
+      mutability: "MUTABLE",
+      data: {
+        url: "https://torchbox.com/blog/rich-text-fields-and-faster-horses/",
+      },
+    },
+    "2": {
+      type: "IMAGE",
+      mutability: "IMMUTABLE",
+      data: {
+        altText: "Test image alt text",
+        alignment: "left",
+        src: "/example.jpg",
+      },
+    },
+    "3": {
+      type: "LINK",
+      mutability: "MUTABLE",
+      data: { href: "https://draftjs.org/", url: "https://draftjs.org/" },
+    },
+  },
 };
 
 export const getInitialContentState = () => {
