ee0993f11eaf24845195bfa62e624de2c7472eeb
Thibaud Colas <thibaudcolas@gmail.com>
2018-11-06 23:46:50 +0200

    Finish adding test cases for MaxLength extension

--- src/setupTests.js
+++ src/setupTests.js
@@ -5,3 +5,10 @@ import { createSerializer } from "enzyme-to-json";
 configure({ adapter: new Adapter() });
 
 expect.addSnapshotSerializer(createSerializer({ mode: "deep" }));
+
+// Throw exceptions of console error messages
+beforeEach(() => {
+  console.error = jest.fn((error) => {
+    throw new Error(error);
+  });
+});
