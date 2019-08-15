cb0ab90733a32325f20c7a9a281210bbab068bd5
Thibaud Colas <thibaudcolas@gmail.com>
2019-08-15 21:20:12 +0100

    Upgrade to latest react-scripts & TypeScript

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "normalize.css";
 import "./index.css";
 
-import "./utils/polyfills";
-
 import App from "./components/App";
-import * as serviceWorker from "./serviceWorker";
 
 const mount = document.getElementById("root");
 
 if (mount) {
   ReactDOM.render(<App />, mount);
 }
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
--- src/utils/polyfills.ts
+++
@@ -1,5 +0,0 @@
-
-/**
- * Add all of the required polyfills in this file.
- */
-import "core-js";
