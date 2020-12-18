a5c5350e822aeffc68397164ce3dd9f0227d6db4
Thibaud Colas <thibaudcolas@gmail.com>
2020-12-18 00:46:55 +0000

    Attempt correct encoding

--- api/export.py
+++ api/export.py
@@ -64,7 +64,23 @@ class handler(BaseHTTPRequestHandler):
         )
 
         self.send_response(200)
-        self.send_header("Content-type", "application/json")
+        self.send_header("Content-type", "application/json; charset=utf-8")
         self.end_headers()
-        self.wfile.write(ret)
+        self.wfile.write(ret.encode("utf8"))
         return
+
+
+# def run(server_class=HTTPServer, handler_class=Server, port=8008):
+#     server_address = ('', port)
+#     httpd = server_class(server_address, handler_class)
+
+#     print 'Starting httpd on port %d...' % port
+#     httpd.serve_forever()
+
+# if __name__ == "__main__":
+#     from sys import argv
+
+#     if len(argv) == 2:
+#         run(port=int(argv[1]))
+#     else:
+#         run()
