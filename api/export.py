3f2d4f4d962672b3632b8ccb0be6bdd32d00bcad
Thibaud Colas <thibaudcolas@gmail.com>
2020-12-18 00:38:43 +0000

    Try out simpler approach to Vercel serverless

---
+++ api/export.py
@@ -0,0 +1,70 @@
+import re
+import json
+from http.server import BaseHTTPRequestHandler
+
+from bs4 import BeautifulSoup
+
+from draftjs_exporter.constants import BLOCK_TYPES, ENTITY_TYPES, INLINE_STYLES
+from draftjs_exporter.defaults import BLOCK_MAP, STYLE_MAP
+from draftjs_exporter.html import HTML
+
+from decorators import import_decorator
+
+from markdown import render_markdown
+
+
+def prettify(markup):
+    return re.sub(
+        r"</?(body|html|head)>",
+        "",
+        BeautifulSoup(markup, "html5lib").prettify(),
+    ).strip()
+
+
+class handler(BaseHTTPRequestHandler):
+    def do_POST(self):
+        try:
+            content_length = int(self.headers["Content-Length"])
+            post_data = self.rfile.read(content_length)
+            request_json = json.loads(post_data)
+        except Exception:
+            self.send_response(400)
+            return
+
+        exporter_config = request_json["exporterConfig"]
+
+        entity_decorators = {}
+        block_map = dict(BLOCK_MAP, **exporter_config.get("block_map", {}))
+        style_map = dict(STYLE_MAP, **exporter_config.get("style_map", {}))
+
+        entity_decorators[ENTITY_TYPES.FALLBACK] = import_decorator(
+            "missing_inline"
+        )
+        block_map[BLOCK_TYPES.FALLBACK] = import_decorator("missing_block")
+        style_map[INLINE_STYLES.FALLBACK] = import_decorator("missing_inline")
+
+        for type_, value in exporter_config.get(
+            "entity_decorators", {}
+        ).items():
+            entity_decorators[type_] = import_decorator(value)
+
+        exporter = HTML(
+            {
+                "entity_decorators": entity_decorators,
+                "block_map": block_map,
+                "style_map": style_map,
+            }
+        )
+
+        html = exporter.render(request_json["contentState"])
+        markdown = render_markdown(request_json["contentState"])
+
+        ret = json.dumps(
+            {"html": html, "markdown": markdown, "prettified": prettify(html)}
+        )
+
+        self.send_response(200)
+        self.send_header("Content-type", "application/json")
+        self.end_headers()
+        self.wfile.write(ret)
+        return
